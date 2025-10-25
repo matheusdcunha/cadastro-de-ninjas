400b48813538d916523d5cce81e5d91adb277d83
Matheus da Cunha <matheus.cunha.fonseca50@gmail.com>
2025-10-25 18:20:34 -0300

    feat: adiciona a entidade de ninjas

---
+++ src/main/java/cloud/matheusdcunha/cadastro_de_ninjas/NinjaModel.java
@@ -0,0 +1,25 @@
+package cloud.matheusdcunha.cadastro_de_ninjas;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "ninjas")
+public class NinjaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+
+    public NinjaModel() {}
+
+    public NinjaModel(Long id, String nome, String email, int idade) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+}
