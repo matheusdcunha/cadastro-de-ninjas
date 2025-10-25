03d63f763c56ada071054189afe98577df2b520d
Matheus da Cunha <matheus.cunha.fonseca50@gmail.com>
2025-10-25 17:20:23 -0300

    feat: adiciona primeira rota

---
+++ src/main/java/cloud/matheusdcunha/cadastro_de_ninjas/Controller.java
@@ -0,0 +1,16 @@
+package cloud.matheusdcunha.cadastro_de_ninjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boas-vindas")
+    public String boasVindas(){
+        return "Olá, boas vindas a essa rota";
+    }
+
+}
